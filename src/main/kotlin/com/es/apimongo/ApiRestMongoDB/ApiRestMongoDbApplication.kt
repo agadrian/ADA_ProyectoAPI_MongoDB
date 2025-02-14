c39af00bed64935eb1d4733417d7c4d42950556e
agadrian <arroyogomezadrian@gmail.com>
2025-02-14 20:56:58 +0100

    First Commit

---
+++ src/main/kotlin/com/es/apimongo/ApiRestMongoDB/ApiRestMongoDbApplication.kt
@@ -0,0 +1,11 @@
+package com.es.apimongo.ApiRestMongoDB
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiRestMongoDbApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiRestMongoDbApplication>(*args)
+}
---
+++ src/test/kotlin/com/es/apimongo/ApiRestMongoDB/ApiRestMongoDbApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.es.apimongo.ApiRestMongoDB
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiRestMongoDbApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
